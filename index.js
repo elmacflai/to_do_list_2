e3ae4cef054aa3d14c032b9e670eb36a06a56fff
Elmacflai <arquitectoalfonso@gmail.com>
2024-04-29 18:26:23 -0600

    added variable deleteButtons no value yet

--- index.js
+++ index.js
@@ -2,3 +2,4 @@ const toDoInput = document.getElementById("toDoInput");
 const addButton = document.getElementById("addButton");
 let toDoItems = []
 const toDoList = document.querySelector(".toDoList");
+let deleteButtons  ;
