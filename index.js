2c6e418fd1d41f755b5cb5f51c065b884df033cd
Elmacflai <arquitectoalfonso@gmail.com>
2024-04-30 19:48:39 -0600

    function render toDos two variables has been added, toDoInput and toDolist, a for cycle to check and select all delete buttons as a click is made

--- index.js
+++ index.js
@@ -3,3 +3,18 @@ const addButton = document.getElementById("addButton");
 let toDoItems = []
 const toDoList = document.querySelector(".toDoList");
 let deleteButtons  ;
+
+function renderToDos(){
+    toDoInput.value = "";
+     toDoList.innerHTML = toDoItems
+     deleteButtons = document.querySelectorAll("#delete") 
+     for (let i=0; i<deleteButtons.length; i++) {
+         deleteButtons[i].addEventListener("click", function(){
+             console.log(toDoItems);
+             toDoItems=toDoItems.filter( function(toDo, index){
+                 return toDo[i]!== toDo[index]
+             })
+             renderToDos();
+         })
+     }
+}
