9b1a786b61e78092dc03fa9ec2761972873a21f3
Elmacflai <arquitectoalfonso@gmail.com>
2024-05-09 21:11:51 -0600

    delete button

--- index.js
+++ index.js
@@ -5,21 +5,11 @@ const toDoList = document.querySelector(".toDoList");
 let deleteButtons  ;
 
 function renderToDos(){
-    toDoInput.value = "";
-     toDoList.innerHTML = toDoItems
-      //Logica para agregar addEventListener al boton 
-      deleteButtons = document.querySelectorAll("#delete") 
-      for (let i=0; i<deleteButtons.length; i++) {
-          deleteButtons[i].addEventListener("click", function(){
-              toDoItems=toDoItems.filter( function(toDo, index){
-                  return toDo[i]!== toDo[index]
-              })
-              renderToDos();
-          })
-      }
+ toDoList.innerHTML = toDoItems.join("");
+ deleteTweets();
 }
 
-function addItems () {
+function addTweets () {
     toDoItems.push(`<li class="toDoItemContainer">${toDoInput.value}
     <div class="buttonContainer">
         <button id="check">Like</button>
@@ -27,9 +17,25 @@ function addItems () {
     </div>
     
    </li>`);
+   toDoInput.value = "";
+   toDoList.innerHTML = toDoItems
    }
    
+function deleteTweets () {
+ //Logica para agregar addEventListener al boton 
+ deleteButtons = document.querySelectorAll("#delete") 
+ for (let i=0; i<deleteButtons.length; i++) {
+     deleteButtons[i].addEventListener("click", function(){
+        //console.log("deleting")
+         toDoItems=toDoItems.filter( function(toDo){
+             return toDo!== toDoItems[i]
+         })
+         renderToDos();
+     })
+ }
+}
 addButton.addEventListener("click", function(){
-        addItems();
+        addTweets();
         renderToDos();   
     })
+
